048ec2bc69be7a0be01b244d5e2cb2c81d546f8d
Cristovao <cristovaofabio3@gmail.com>
2022-04-02 09:34:52 -0300

    check logged user

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contactController = require('./src/controllers/contactController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 //home routes:
 route.get('/', homeController.index);
@@ -11,5 +14,8 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+//contact routes:
+route.get('/contact/index', loginRequired, contactController.index);
 
 module.exports = route;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -9,12 +9,21 @@ exports.checkCsrfError = (err, req, res, next) => {
     if (err) {
         return res.render('404');
     }
-    
+
     next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
-  };
+};
 
+exports.loginRequired = (req, res, next) => {
+    if (!req.session.user) {
+        req.flash('errors', 'You need to login');
+        req.session.save(() => res.redirect('/'));
+        return;
+    }
+
+    next();
+}
